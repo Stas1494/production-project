fe0cf684916a901f986928daf90405ee0c55a8b7
Stas1494 <stason.1494@yandex.ru>
2023-02-15 00:15:58 +0500

    add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,19 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
@@ -15,7 +28,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
                         auto: (resPath: string) => Boolean(resPath.includes('.module.')),
                         localIdentName: isDev
                             ? '[path][name]__[local]--[hash:base64:5]'
-                            :'[hash:base64:8]'
+                            : '[hash:base64:8]'
                     },
                 }
             },
@@ -29,7 +42,30 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            "keyAsDefaultValue": true
+                        }
+                    ],
+                ]
+            }
+        }
+    }
+
     return [
+        fileLoader,
+        svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ]
