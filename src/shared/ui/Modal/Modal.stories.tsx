75fdbd7b5fe2e0c99c9cb5cc70077c3f0b186cbf
Stas1494 <stason.1494@yandex.ru>
2023-02-28 12:16:36 +0500

    add modal, portal, screenshots test for modal and button, change header

---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import 'app/styles/index.scss';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Modal } from './Modal';
+
+export default {
+    title: 'shared/Modal',
+    component: Modal,
+
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    isOpen: true,
+    children: 'Это не финальное состояние модального окна, в ходе всего курса мы будем его менять рефакторить. Также прошу заметить, что переопределен\n',
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+    isOpen: true,
+    children: 'Это не финальное состояние модального окна, в ходе всего курса мы будем его менять рефакторить. Также прошу заметить, что переопределен\n',
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
