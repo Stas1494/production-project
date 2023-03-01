6579d19b3bae4e29b9be64ac9f44cf238fc80cdd
Stas1494 <stason.1494@yandex.ru>
2023-03-01 15:22:35 +0500

    add redux store, add first entity

---
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -0,0 +1,23 @@
+import { Provider } from 'react-redux';
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { ReactNode } from 'react';
+import { DeepPartial } from '@reduxjs/toolkit';
+import { createReduxStore } from '..';
+
+interface StoreProviderProps {
+    children?: ReactNode;
+    initialState?: DeepPartial<StateSchema>;
+}
+
+export const StoreProvider = (props: StoreProviderProps) => {
+    const {
+        children,
+        initialState,
+    } = props;
+    const store = createReduxStore(initialState as StateSchema);
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    );
+};
