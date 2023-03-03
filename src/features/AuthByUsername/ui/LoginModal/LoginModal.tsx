925e3878ea862d77e7e3c59c33789e289e979e5c
Stas1494 <stason.1494@yandex.ru>
2023-03-03 17:01:02 +0500

    add pre-commit

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,13 +1,16 @@
+import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
 
 interface LoginModalProps {
+    className?: string,
     isOpen: boolean,
     onClose: () => void
 }
 
-export const LoginModal = ({ isOpen, onClose }: LoginModalProps) => (
+export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
     <Modal
+        className={classNames('', {}, [className])}
         isOpen={isOpen}
         onClose={onClose}
         lazy
