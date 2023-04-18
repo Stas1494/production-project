da69cb2693395d7332e1c39f7237b86773b87ae2
Stas1494 <stason.1494@yandex.ru>
2023-04-18 15:46:49 +0500

    add new comment list,

---
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -0,0 +1,38 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import { Text } from 'shared/ui/Text/Text';
+import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
+import { CommentCard } from '../CommentCard/CommentCard';
+import cls from './CommentList.module.scss';
+import { Comment } from '../../model/types/comment';
+
+interface CommentListProps {
+    className?: string;
+    comments?: Comment[];
+    isLoading?: boolean;
+}
+
+export const CommentList = memo((props: CommentListProps) => {
+    const {
+        className,
+        comments,
+        isLoading,
+    } = props;
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.CommentList, {}, [className])}>
+            {comments?.length
+                ? comments.map((comment) => (
+                    <CommentCard
+                        isLoading={isLoading}
+                        className={cls.comment}
+                        comment={comment}
+                        key={comment.id}
+                    />
+                ))
+                : <Text title={t('Нет комментариев')} />}
+        </div>
+    );
+});
--- src/pages/ArticleDetailsPage/index.ts
+++ src/pages/ArticleDetailsPage/index.ts
@@ -1,3 +1,7 @@
 export {
     ArticleDetailsPageAsync as ArticleDetailsPage,
 } from './ui/ArticleDetailsPage.async';
+
+export { ArticleDetailsCommentsSchema } from './model/types/ArticleDetailsCommentsSchema';
+export { getArticleCommentsIsLoading } from './model/selectors/comments';
+export { getArticleCommentsError } from './model/selectors/comments';
