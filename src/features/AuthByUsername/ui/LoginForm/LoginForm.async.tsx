9e7da55212cff0544eb00e9229ba5b7a6ba3e364
Stas1494 <stason.1494@yandex.ru>
2023-05-23 15:00:37 +0500

    add screen

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.async.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.async.tsx
@@ -1,8 +1,4 @@
 import { lazy, FC } from 'react';
 import { LoginFormProps } from './LoginForm';
 
-export const LoginFormAsync = lazy <FC<LoginFormProps>>(() => new Promise((resolve) => {
-    // @ts-ignore
-    // Задержка для курса, позже УДАЛИТЬ
-    setTimeout(() => resolve(import('./LoginForm')), 1500);
-}));
+export const LoginFormAsync = lazy <FC<LoginFormProps>>(() => import('./LoginForm'));
