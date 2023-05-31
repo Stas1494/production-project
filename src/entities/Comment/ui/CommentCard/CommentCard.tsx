b27cf5cacf61734a5024d4cdac527525be8ed605
Stas1494 <stason.1494@yandex.ru>
2023-06-01 03:03:03 +0500

    fix style problems

--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -21,13 +21,13 @@ export const CommentCard = memo((props: CommentCardProps) => {
     const { t } = useTranslation();
     if (isLoading) {
         return (
-            <div className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
+            <VStack gap="8" max className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
                 <div className={cls.header}>
                     <Skeleton width={30} height={30} border="50%" />
                     <Skeleton width={100} height={16} />
                 </div>
                 <Skeleton width="100%" height={50} className={cls.text} />
-            </div>
+            </VStack>
         );
     }
     if (!comment) return null;
