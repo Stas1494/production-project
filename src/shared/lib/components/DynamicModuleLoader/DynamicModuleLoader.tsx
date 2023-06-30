8585f64cb5bc76298dfa6faf8acc6fa2b3bad877
Stas1494 <stason.1494@yandex.ru>
2023-06-30 17:35:04 +0500

    fix reducer list

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -1,10 +1,10 @@
 import React, { ReactNode, useEffect } from 'react';
 import { useDispatch, useStore } from 'react-redux';
 import { Reducer } from '@reduxjs/toolkit';
-import { reduxStoreWithManager, StateSchemaKey } from '@/app/providers/StoreProvider';
+import { reduxStoreWithManager, StateSchema, StateSchemaKey } from '@/app/providers/StoreProvider';
 
 export type ReducersList = {
-    [name in StateSchemaKey]?: Reducer;
+    [name in StateSchemaKey]?: Reducer<NonNullable<StateSchema[name]>>;
 }
 
 interface DynamicModuleLoaderProps {
